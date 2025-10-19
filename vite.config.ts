2024df955a3c442876bf778b9c878f4413f4b510
reaksa-maii <maireaska@gmail.com>
2025-10-19 21:24:40 +0700

    add Dockerfile build release tag

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['sovina-testing.itedev.online']
+  },
   plugins: [
     react(),
     tailwindcss(),
