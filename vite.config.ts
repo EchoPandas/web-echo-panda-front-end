42488bfa0a2507f798c5b380b1d14b71f751790c
Mai Reaksa <mai.reaksa@sabay.com>
2025-10-20 08:07:57 +0700

    build update pipline

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['sovina-testing.itedev.online']
+    allowedHosts: ['echo-panda.itedev.online']
   },
   plugins: [
     react(),
