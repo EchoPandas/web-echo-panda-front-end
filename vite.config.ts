4b6c0181269a8853a02c39b4ee463a98ac896fe5
Mai Reaksa <mai.reaksa@sabay.com>
2025-10-20 08:14:05 +0700

    update host naem

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { hostname } from 'os'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    hostname: '0.0.0.0',
     allowedHosts: ['echo-panda.itedev.online']
   },
   plugins: [
