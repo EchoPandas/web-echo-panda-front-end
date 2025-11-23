e17ee9a455dd8256cdeba290946f5ef1da707b64
reaksa-maii <maireaska@gmail.com>
2025-11-23 08:33:07 +0700

    commend config taiwindcss import defult

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/main.css'
 import App from './App'
+import "tailwindcss"
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
